5b9b9c259dd509454a11d2d71af9ed3cd57a8782
KennethCruz <30338645+Piecre24@users.noreply.github.com>
2020-12-16 22:23:24 +1300

    added babel, scripts and indexs and server for both sides

--- server/server.js
+++ server/server.js
@@ -1,13 +1,10 @@
-const express = require('express')
 const path = require('path')
-
-const fruitRoutes = require('./routes/fruits')
+const express = require('express')
 
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
-server.use('/api/v1/home', fruitRoutes)
 
 module.exports = server
