3aa7f63355ff2ccd49f744884a34ed7721769d4c
KennethCruz <30338645+Piecre24@users.noreply.github.com>
2020-12-18 16:21:59 +1300

    added more dependecies but webpack not workin

---
+++ client/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+
+export class App extends React.Component {
+  render() {
+    return (
+      <>
+        <body>
+          <Test />
+        </body>
+      </>
+    )
+  }
+}
+
+export default App
---
+++ client/components/Test.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Test = () => {
+  return (
+    <>
+      <footer className="footer">
+        <div className="content has-text-centered">
+          <p>
+            <strong>Created</strong> by <strong>Field, Gio, Jack, Gavin.</strong> Enspiral Dev Academy. 2020 ©.
+          </p>
+        </div>
+      </footer>
+    </>
+  )
+}
+export default Test
