012cb6710c1aa9f439717759c1261415dab384dc
KennethCruz <30338645+Piecre24@users.noreply.github.com>
2021-03-13 15:12:15 +1300

    added content from home to aboutme

--- client/components/home.jsx
+++ client/components/home.jsx
@@ -8,29 +8,6 @@ export class home extends React.Component {
           <h2>Hi I’m Kenneth Cruz
           Developer</h2>
           <p>This is Website consist of <a href="https://reactjs.org/" className='reactLink'>React</a> and  <a href="https://redux.js.org/" className='reduxLink'>Redux </a></p>
-          <h3>About Me</h3>
-          <p>Full name is Kenneth Capistrano Cruz, born in the Philippines but raised in New Zealand often find myself talk more English than my native language. Though I am often curious about things I’m no knowledge of and would attempt to learn it in full details.</p>
-
-          <p>I enjoy delivering new ideas to the table and being able to create them from scratch.
-             Ideas that would revolutionize the industry or society, in general, are something
-             I'm quite interesting to be a part of. I'm told that I'm being creative and smart, I'm passionate about designing and was painting arts and crafts when I was young.
-             Now taking a step further, the to design Tech application.
-          </p>
-
-          <p>
-          Designing my own website portfolio, consist basically of my skills set and experience
-          about myself that will involve projects that I have to create or partake in. In the
-           future date, it will have a blog functionality that will post updates about myself
-           currently. Been also delving into micro-computers learning C# in curiosity, to know
-           more about software engineering.
-          </p>
-          <p>
-          Hatching unique ideas is my specialty, often would give concepts people haven't talked
-          about or yet to be popular. My ideas also bring smiles and laughter to my fellow workers
-          as I've been told. Though Ideas are just concepts and not productive, often during my
-          learning, I’ve been told that I’ve asked several questions to ensure that my understanding
-          is in detail.
-          </p>
 
         </div>
 
