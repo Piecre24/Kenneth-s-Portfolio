5c35f08af80145751a0a6bf2c03f778842e56d4c
KennethCruz <30338645+Piecre24@users.noreply.github.com>
2021-03-14 18:13:53 +1300

    added database directory and knexfile connections

---
+++ server/db/migrations/seeds/knexfile.js
@@ -0,0 +1,40 @@
+const path = require('path')
+
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: path.join(__dirname, 'dev.sqlite3')
+    },
+    useNullAsDefault: true
+  },
+
+  test: {
+    client: 'sqlite3',
+    connection: {
+      filename: ':memory:'
+    },
+    useNullAsDefault: true,
+    seeds: {
+      directory: path.join(__dirname, 'tests/seeds')
+    },
+    migrations: {
+      directory: path.join(__dirname, 'migrations')
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: path.join(__dirname, 'migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'seeds')
+    }
+  }
+}
